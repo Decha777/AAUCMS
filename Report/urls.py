2febfaa69882f5c5d1cd9353d3fcc8d091b9c7b7
thenewcreation <galatafir@yahoo.com>
2020-11-05 16:44:45 +0300

    merge with ui

--- Report/urls.py
+++ Report/urls.py
@@ -6,6 +6,6 @@ app_name = 'Report'
 
 urlpatterns = [
     path('tasks', views.list_activities, name='activity'),
-    path('create_activity',views.create_activity)
+    path('create_activity',views.create_activity),
     path('', views.report, name='home')
 ]
